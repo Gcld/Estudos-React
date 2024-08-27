a665ec6c312f481166b4152826f1086de0449de2
gabriel.lima <gabrielcostalima21@gmail.com>
2024-08-27 10:55:12 -0300

    Aula 11

--- projeto1/src/pages/index.tsx
+++ projeto1/src/pages/index.tsx
@@ -25,10 +25,10 @@ export default function Home() {
           <div style={{color:'#f00', backgroundColor:'#bbb'}}>React</div>
       </div>
       <div className='flex justify-center gap-3'>
-        <Card produto={'Mouse'} valor={49.90} desconto = {10} funcao={calcDesc}/>
-        <Card produto={'Teclado'} valor={69.90} desconto = {2} funcao={calcDesc}/>
-        <Card produto={'Monitor'} valor={459.90} desconto = {0} funcao={calcDesc2}/>
-        <Card produto={'CPU'} valor={759.90} desconto = {50} funcao={calcDesc2}/>
+        <Card produto={'Mouse'} valor={49.90} desconto = {0} funcao={calcDesc}/>
+        <Card produto={'Teclado'} valor={69.90} desconto = {0} funcao={calcDesc}/>
+        <Card produto={'Monitor'} valor={459.90} desconto = {100} funcao={calcDesc2}/>
+        <Card produto={'CPU'} valor={759.90} desconto = {0} funcao={calcDesc2}/>
       </div>
     </div>
   );
