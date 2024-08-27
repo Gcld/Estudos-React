b653c279d6f4128957f6d183ecc73a3948495395
gabriel.lima <gabrielcostalima21@gmail.com>
2024-08-27 10:22:41 -0300

    Aula 8

--- projeto1/src/pages/index.tsx
+++ projeto1/src/pages/index.tsx
@@ -3,6 +3,7 @@
 // const inter = Inter({ subsets: ["latin"] });
 
 import Topo from '@/components/Topo'
+import Card from '@/components/Card';
 
 const nome='Gabriel'
 let empresa='Zaori'
@@ -15,6 +16,12 @@ export default function Home() {
           <div>Curso de react Next</div>
           <div>Typescript</div>
           <div style={{color:'#f00', backgroundColor:'#bbb'}}>React</div>
+      </div>
+      <div className='flex justify-center gap-3'>
+        <Card produto={'Mouse'} valor={'R$49,90'}/>
+        <Card produto={'Teclado'} valor={'R$69,90'}/>
+        <Card produto={'Monitor'} valor={'R$459,90'}/>
+        <Card produto={'CPU'} valor={'R$759,90'}/>
       </div>
     </div>
   );
