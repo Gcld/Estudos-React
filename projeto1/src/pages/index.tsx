2f77af4a37fa5f65fb7c8db359dc479b7d887c2c
gabriel.lima <gabrielcostalima21@gmail.com>
2024-08-27 10:13:46 -0300

    Aula 7

--- projeto1/src/pages/index.tsx
+++ projeto1/src/pages/index.tsx
@@ -2,30 +2,19 @@
 // import { Inter } from "next/font/google";
 // const inter = Inter({ subsets: ["latin"] });
 
+import Topo from '@/components/Topo'
+
 const nome='Gabriel'
 let empresa='Zaori'
-
-function Topo(){
-  return (
-    <div className='flex justify-between items-center bg-zinc-300 h-[100px]'>   
-      <div>Logo</div>
-      <div className='flex flex-col justify-center items-center'>
-        <div className='text-3xl'>{empresa}</div>
-        <div className='subtituloTopo'>Curso de React</div>
-      </div>
-      <div>{nome}</div>
-    </div>
-  )
-}
 
 export default function Home() {
     return (
       <div>
-      <Topo/>
-      <div style={testecss}>
-        <div>Curso de react Next</div>
-        <div>Typescript</div>
-        <div style={{color:'#f00', backgroundColor:'#bbb'}}>React</div>
+        <Topo/>
+        <div style={testecss}>
+          <div>Curso de react Next</div>
+          <div>Typescript</div>
+          <div style={{color:'#f00', backgroundColor:'#bbb'}}>React</div>
       </div>
     </div>
   );
