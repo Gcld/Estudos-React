65a599ce21e4c7e0f479181b91f316b47fa364a5
gabriel.lima <gabrielcostalima21@gmail.com>
2024-09-04 08:30:50 -0300

    Aula 14

--- projeto1/src/pages/produtos/produtos.tsx
+++ projeto1/src/pages/produtos/produtos.tsx
@@ -1,38 +1,45 @@
 import Card from "@/components/Card"
 import Topo from "@/components/Topo"
+import { useRouter } from "next/router"
 
 const produtos=[
     {
+        id:1,
         produto:'Mouse',
         valor:49.90,
         desconto:0,
         disponivel: true
     },
     {
+        id:2,
         produto:'Teclado',
         valor:69.90,
         desconto:10,
         disponivel: true
     },
     {
+        id:3,
         produto:'Monitor',
         valor:649.90,
         desconto:0,
         disponivel: true
     },
     {
+        id:4,
         produto:'CPU',
         valor:829.9,
         desconto:100,
         disponivel: true
     },
     {
+        id:5,
         produto:'Cx.Som',
         valor:39.90,
         desconto:0,
         disponivel: true
     },
     {
+        id:6,
         produto:'Microfone',
         valor:65.0,
         desconto:0,
@@ -49,6 +56,13 @@ function calcDesc2(v:number, d:number){
 }
 
 export default function produtosPagina(){
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const router=useRouter()
+    // const nome=router.query.nome
+    // const curso=router.query.curso
+    const {nome, curso}=router.query
+    console.log(nome)
+    console.log(curso)
     return(     
         <div>
             <Topo/>
@@ -58,7 +72,7 @@ export default function produtosPagina(){
                 if(e.disponivel)
                 return(
                     // eslint-disable-next-line react/jsx-key
-                    <Card produto={e.produto} valor={e.valor} desconto={e.desconto} funcao={calcDesc} />
+                    <Card key ={e.id} produto={e.produto} valor={e.valor} desconto={e.desconto} funcao={calcDesc} />
                 )
             })
             }
